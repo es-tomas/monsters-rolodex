529927c04fec0700ec35ed3cbe4e4c2b4f60b269
Tomas Escamez <tomase@gmail.com>
2021-05-08 12:40:42 +1200

    adding files for github pages

---
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import './search-box.style.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <div className='search'>
+        <input
+            type='search'
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    </div >
+)
